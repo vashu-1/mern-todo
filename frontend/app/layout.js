62c7c01ef05612d96654c559d7e22b883bfbfee8
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-11-02 13:29:46 +0530

    solved

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -5,11 +5,15 @@ import ReduxProvider from './providers/ReduxProvider';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
+  display: 'swap',
+  preload: true,
 });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+  display: 'swap',
+  preload: true,
 });
 
 export const metadata = {
